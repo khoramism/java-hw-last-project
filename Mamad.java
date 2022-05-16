176d9628735fcb43f57bd750874b977a38f0e17a
khoramism@homaplus <khoramism@gmail.com>
2022-05-16 20:08:06 +0430

    fixing date problem, adding Dao func

---
+++ Mamad.java
@@ -0,0 +1,14 @@
+
+import java.util.Calendar;
+import java.util.Date;
+import java.text.SimpleDateFormat;
+public class Mamad {
+    public static void main(String[] args) {
+        SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+        Date created = Calendar.getInstance().getTime();  
+        System.out.println(created);
+        Date dateCreated = new Date();
+        
+        System.out.println(dateCreated);
+    }
+}
