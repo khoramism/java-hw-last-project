bfec9e041a71d3716b021fbdb4b9c5d7272a41f9
dara100ry <96252204+dara100ry@users.noreply.github.com>
2022-05-24 17:50:54 +0430

    Create login.java

---
+++ UI/loginPage/login.java
@@ -0,0 +1,29 @@
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+public class Login {
+
+    @FXML
+    private Button btnSabt;
+
+    @FXML
+    private Label txtLable1;
+
+    @FXML
+    private Label txtLable2;
+
+    @FXML
+    private TextField txtPassword;
+
+    @FXML
+    private TextField txtUsername;
+
+    @FXML
+    void pressBtnSabt(ActionEvent event) {
+
+    }
+
+}
